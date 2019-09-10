688ec275ba9910ef9dac4174b80089e2507c1b4d
Steve Min <stevekmin@gmail.com>
2019-09-10 00:01:39 +0000

    add getAverage() to app component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -26,9 +26,23 @@ export default class App extends React.Component {
         })
         .catch(error => console.error('error: ', error));
   }
+  getAverage() {
+    let inputState = this.state.userInput;
+    let totalInputs = 0;
+    for (input of inputState) {
+      totalInputs += parseInt(input.calories);
+    }
+    let average = totalInputs / inputState.length;
+    let total = average.toFixed(2);
+    if (isNaN(total)) {
+      return 'N/A';
+    }
+    return total;
+  }
   render() {
+    let average = this.getAverage();
     return (
-      <Header/>
+      <Header average={average}/>
     );
   }
 }
