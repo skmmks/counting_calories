025ee60203efe7e37be3fd9442c6d490f163f32d
Steve Min <stevekmin@gmail.com>
2019-09-10 04:40:02 +0000

    add userInput component import reactstrap into userInput component setup initial props and state for userInput componenent

---
+++ client/components/userinput.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap';
+
+export default class userInput extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+}
