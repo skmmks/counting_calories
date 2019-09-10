acee40cac042e1596d1b7cfd6439a2a878081d8c
Steve Min <stevekmin@gmail.com>
2019-09-10 04:43:51 +0000

    create inputForm component setup initial state and props for inputForm component

--- client/components/user-input.jsx
+++ client/components/user-input.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap';
 
-export default class userInput extends React.Component {
+export default class UserInput extends React.Component {
     constructor(props) {
         super(props);
     }
