fc45bcd80d8eefe97f196e46a0b73e146b9b77e9
Steve Min <stevekmin@gmail.com>
2019-09-10 01:03:40 +0000

    add css style for header component add css class to header component

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,8 +5,8 @@ function Header(props) {
     return (
         <Row>
             <img src="images/smoothies.png" alt="smoothie logo" className="logo"/>
-            <Col> </Col>
-            <Col>Average: <Badge >{props.average} cal</Badge></Col>
+            <Col className="title">The Helpful Calorie Counter</Col>
+            <Col sm="4" className="average">Average: <Badge className="badge" color="success">{props.average} cal</Badge></Col>
         </Row>
     )
 }
