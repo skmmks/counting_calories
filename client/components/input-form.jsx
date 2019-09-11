0e0c0bdcfad1e237c1abc15c1265f781e8acc368
Steve Min <stevekmin@gmail.com>
2019-09-11 05:06:58 +0000

    create initial input form

--- client/components/input-form.jsx
+++ client/components/input-form.jsx
@@ -19,8 +19,29 @@ export default class InputForm extends React.Component {
                 <div>
                     <FormGroup>
                         <InputGroup>
-                            <InputGroupAddon addonType="prepend" className="input-group-text" />
-                            <Input type="text"/>
+                            <InputGroupAddon addonType="prepend" className="input-group-text fas fa-utensils" />
+                            <Input
+                                type="text"
+                                placeholder="MealTime"
+                            />
+                        </InputGroup>
+                    </FormGroup>
+                    <FormGroup>
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend" className="input-group-text fas fa-hotdog" />
+                            <Input
+                                type="text"
+                                placeholder="What did you Eat"
+                            />
+                        </InputGroup>
+                    </FormGroup>
+                    <FormGroup>
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend" className="input-group-text fas fa-weight" />
+                            <Input
+                                type="text"
+                                placeholder="Calories"
+                            />
                         </InputGroup>
                     </FormGroup>
                 </div>
