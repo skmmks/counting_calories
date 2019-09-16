321fd6b3d396fae45ee76a195fbc39a3f8c43113
Steve Min <stevekmin@gmail.com>
2019-09-16 20:34:10 +0000

    remove commented code from header component

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -12,13 +12,3 @@ function Header(props) {
 }
 
 export default Header;
-// export default class Header extends React.Component {
-//   render() {
-//     return (
-//       <div className={'row'}>
-//         <h1 className={'col-8'}>Counting Calories</h1>
-//         <h5 className={'col-4'}>Average Calorie Intake: {props.average}</h5>
-//       </div>
-//     );
-//   }
-// }
