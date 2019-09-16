23e383352b510f6ec9b1f3f2bfb7db3dc6cbda01
Steve Min <stevekmin@gmail.com>
2019-09-16 20:39:55 +0000

    add header css

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,7 @@ import { Row, Col, Badge } from 'reactstrap';
 
 function Header(props) {
     return (
-        <Row>
+        <Row className="header">
             <img src="images/smoothies.png" alt="smoothie logo" className="logo"/>
             <Col className="title">The Helpful Calorie Counter</Col>
             <Col sm="4" className="average">Average: <Badge className="badge" color="success">{props.average}</Badge></Col>
