1ad6fe10492d4ad4f06b3034a77faf5a445862fc
Steve Min <stevekmin@gmail.com>
2019-09-26 00:05:51 +0000

    revert webpack config add dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,42 @@
-const fs = require('fs');
+require('dotenv/config');
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '/../.env') });
-const jsonServer = require('json-server');
 
-const dbPath = path.resolve(__dirname, '../database/db.json');
-const server = jsonServer.create();
-const middleware = jsonServer.defaults();
-const endpoints = jsonServer.router(dbPath);
+const srcPath = path.resolve(__dirname, 'client');
+const publicPath = path.resolve(__dirname, 'server/public');
 
-const data = require(dbPath);
-const db = endpoints.db;
-
-setInterval(() => {
-  fs.writeFile(dbPath, JSON.stringify(data, null, 2), () => {
-    db.read();
-  });
-}, 1000 * 60 * 5);
-
-server.use(middleware);
-server.use('/api', endpoints);
-server.listen(process.env.PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log('JSON Server listening on port', process.env.PORT);
-});
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  entry: './client',
+  output: {
+    path: publicPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: srcPath,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@babel/plugin-transform-react-jsx'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    host: '0.0.0.0',
+    port: process.env.DEV_PORT,
+    contentBase: publicPath,
+    watchContentBase: true,
+    stats: 'minimal',
+    proxy: {
+      '/api': 'http://localhost:' + process.env.PORT
+    }
+  }
+};
