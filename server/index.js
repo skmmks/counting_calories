6d6bae4204d961c089d3529c927f35ca721a1c1c
Steve Min <stevekmin@gmail.com>
2019-09-26 00:00:37 +0000

    update git ignore add env update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '/../.env') });
 const jsonServer = require('json-server');
 
 const dbPath = path.resolve(__dirname, '../database/db.json');
@@ -6,9 +8,18 @@ const server = jsonServer.create();
 const middleware = jsonServer.defaults();
 const endpoints = jsonServer.router(dbPath);
 
+const data = require(dbPath);
+const db = endpoints.db;
+
+setInterval(() => {
+  fs.writeFile(dbPath, JSON.stringify(data, null, 2), () => {
+    db.read();
+  });
+}, 1000 * 60 * 5);
+
 server.use(middleware);
 server.use('/api', endpoints);
-server.listen(3001, () => {
+server.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('JSON Server listening on port 3001\n');
+  console.log('JSON Server listening on port', process.env.PORT);
 });
